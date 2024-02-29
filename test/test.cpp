f886ebf8eae78537d455fd5b143ef6c1c1b1226d
Tzec557 <ezhao012@ucr.edu>
2024-02-28 18:08:17 -0800

    draft of test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,25 @@
+
+/*#include "../header/Character.hpp"
+#include <gtest/gtest.h>
+
+//constructor
+
+TEST(Constructors, ConstructorTest) {
+    Rectangle rectangle(3, 4);
+    EXPECT_EQ(rectangle.area(), 12);
+    EXPECT_EQ(rectangle.perimeter(), 14);
+}
+
+TEST(Constructors, ConstructorWithZerosTest) {
+    Rectangle rectangle(0, 0);
+    EXPECT_EQ(rectangle.area(), 0);
+    EXPECT_EQ(rectangle.perimeter(), 0);
+}
+
+TEST(Constructors, ConstructorWithNegativeValuesTest) {
+    Rectangle rectangle(-2, 5);
+    EXPECT_EQ(rectangle.area(), -10); 
+    EXPECT_EQ(rectangle.perimeter(), 6);
+}
+
+*/
