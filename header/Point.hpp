d30f6fb618419a0248774381392e6b00f2eb16df
Nathan Tewinpagti <ntewi001@xe-01.cs.ucr.edu>
2024-02-28 18:00:29 -0800

    point class

---
+++ header/Point.hpp
@@ -0,0 +1,18 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+using namespace std;
+
+class Point{
+    public:
+        Point();
+        ~Point();
+
+        int getPoint();
+        void addPoint(int num);
+
+
+    private:
+        int pointNum;
+
+};
+ #endif
