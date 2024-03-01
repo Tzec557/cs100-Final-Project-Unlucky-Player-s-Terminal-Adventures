52625941dea5e616ce46aa1fdeae45d182625043
Tzec557 <ezhao012@ucr.edu>
2024-02-29 19:30:08 -0800

    modify Character class

--- src/Enemy.cpp
+++
@@ -1,9 +0,0 @@
-#include"../header/Enemy.hpp"
-#include<iostream>
-using namespace std;
-
-Enemy::Enemy() : Character(Boss){}
-
-void Enemy::damageTaken(int num){
-   _health = _health + num;
-}
--- src/Player.cpp
+++
@@ -1,9 +0,0 @@
-#include"../header/Player.hpp"
-#include<iostream>
-using namespace std;
-
-Player::Player() : Character(Player){}
-
-void Player::damageDone(int num){
-   _health = _health + num;
-}
