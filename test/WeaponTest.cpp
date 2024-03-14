dacad981630a91ff639c6e7793b0b1998d40de30
Nathan Tewinpagti <ntewi001@xe-01.cs.ucr.edu>
2024-03-13 22:06:09 -0700

    weapon_test

--- test/WeaponTest.cpp
+++ test/WeaponTest.cpp
@@ -5,7 +5,7 @@ TEST(WeaponTests, testinitializer){
     Weapon *t = new Weapon();
     ASSERT_NE(t,nullptr);
 }
-TEST(PlayerTests, batChangeWeapon){
+TEST(WeaponTests, batChangeWeapon){
     Point* tPoint = new Point();
     Weapon *t = new Weapon();
 
@@ -14,7 +14,7 @@ TEST(PlayerTests, batChangeWeapon){
     ASSERT_EQ(t->getWeapon(),"bat");
     ASSERT_EQ(t->getWeaponDamage(), -12);
 }
-TEST(PlayerTests, steelBatChangeWeapon){
+TEST(WeaponTests, steelBatChangeWeapon){
     Point* tPoint = new Point();
     Weapon *t = new Weapon();
 
@@ -23,7 +23,7 @@ TEST(PlayerTests, steelBatChangeWeapon){
     ASSERT_EQ(t->getWeapon(),"steel bat");
     ASSERT_EQ(t->getWeaponDamage(), -26);
 }
-TEST(PlayerTests, swordChangeWeapon){
+TEST(WeaponTests, swordChangeWeapon){
     Point* tPoint = new Point();
     Weapon *t = new Weapon();
 
@@ -32,7 +32,7 @@ TEST(PlayerTests, swordChangeWeapon){
     ASSERT_EQ(t->getWeapon(),"sword");
     ASSERT_EQ(t->getWeaponDamage(), -36);
 }
-TEST(PlayerTests, lightSaberChangeWeapon){
+TEST(WeaponTests, lightSaberChangeWeapon){
     Point* tPoint = new Point();
     Weapon *t = new Weapon();
 
